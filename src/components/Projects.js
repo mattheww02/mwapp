659b359e675156e33470b92173fa1f019e9464b7
mattheww02 <78615724+mattheww02@users.noreply.github.com>
2024-09-26 01:01:32 +0100

    Fixed modal spacing

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -20,10 +20,10 @@ const Projects = () => {
              
                 <div class="row justify-content-center">
                
-                    <div class="col-md-6 col-lg-4">
+                    <div class="col-md-6 col-lg-4 mb-5">
                         <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#modal7">
                             <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                <div class="portfolio-item-caption-content text-center text-white">Chess Engine</div>
+                                <div class="portfolio-item-caption-content text-center text-white">Automated Image Geolocation</div>
                             </div>
                             <img class="img-fluid" src={chessImg} alt="..." />
                         </div>
